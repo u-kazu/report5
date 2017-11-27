cbd92b07b930b3b9e42239395c48ed52d11030e5
kazuma Uehara <e175740@ie.u-ryukyu.ac.jp>
2017-11-27 16:53:26 +0900

    step2 commit

--- IdeaProjects/Report4/src/Report4.java
+++ IdeaProjects/Report4/src/Report4.java
@@ -1,7 +1,11 @@
 public class Report4 {
-    public static void main(String[] args){
-       String str = null;
-       System.out.println(str.length());
+    public static void main(String[] args) {
+        String str = null;
+        try {
+            System.out.println(str.length());
+        }catch (NullPointerException e) {
+            System.out.println("NullPointerExceptionが発生しました");
+            System.out.println(e.getMessage());
+        }
     }
-
 }
